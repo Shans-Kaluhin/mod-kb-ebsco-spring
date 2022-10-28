9581e6142e2a67a96ae6881a2b06ab203fe3d9e5
Shans Kaluhin <shans_kaluhin@epam.com>
2022-10-28 07:46:46 +0300

    add unused import

--- src/main/java/org/folio/KbEbscoApplication.java
+++ src/main/java/org/folio/KbEbscoApplication.java
@@ -1,7 +1,8 @@
 package org.folio;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class KbEbscoApplication {
